1051044a53e37b3f9ae09167686d8687cb47a1d0
Nikita Tank <67931401+nikitatank@users.noreply.github.com>
2020-10-02 12:35:29 -0700

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,7 +13,7 @@ const users = [];
 
 const siteConfig = {
     title: 'Nikita Tank', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    tagline: ' Continuously improving, aspiring technologist. ',
     profilePhoto: 'img/profile.png',
     url: 'https://nikitatank.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
